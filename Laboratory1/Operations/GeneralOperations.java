71b817d729c22924a5376a9ca82637b30cdb53fc
XRRRA <liviu.iordan@isa.utm.md>
2023-10-01 21:04:36 +0300

    GeneralOperations and FacultyOperations main functions added

--- Laboratory1/Operations/GeneralOperations.java
+++ Laboratory1/Operations/GeneralOperations.java
@@ -1,12 +1,15 @@
 package Laboratory1.Operations;
 
 import Laboratory1.Faculty;
+import Laboratory1.Student;
 import Laboratory1.StudyField;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class GeneralOperations {
+    private static final List<Student> students = Student.getStudentsList();
     private static final List<Faculty> faculties = Faculty.getFacultyList();
     public static void generalOperations(Scanner input) {
         Scanner scan = new Scanner(System.in);
@@ -24,9 +27,8 @@ public class GeneralOperations {
             System.out.println();
             System.out.print("Your input: ");
             String choice = input.nextLine().trim();
-
+            String[] parts = choice.split("/");
             if (choice.startsWith("nf/")) {
-                String[] parts = choice.split("/");
                 if (parts.length == 4) {
                     String facultyName = parts[1];
                     String facultyAbbreviation = parts[2];
@@ -41,13 +43,26 @@ public class GeneralOperations {
                     decision = decision.toLowerCase();
                     if (decision.equals("n")) System.exit(0);
                 }
-            }
-            //TODO Search the student by email and show the faculty he's from
-
-
-
-
-            else if (choice.equals("df")){
+            } else if (choice.startsWith("ss/")) {
+                if (parts.length == 2) {
+                    String email = choice.substring(3);
+                    for (Student student : students) {
+                        if (student.getEmail().equals(email)) {
+                            System.out.println("Student found:");
+                            System.out.println(student.getFirstName() + " " + student.getLastName());
+                            System.out.println("Belongs to faculty: " + student.getFacultyAbbreviation());
+                            return;
+                        }
+                    }
+                    System.out.println("Student with email " + email + " not found.");
+                } else {
+                    System.out.println("Invalid input for creating a faculty. Please follow the format. Do you want to try again? y/n");
+                    System.out.print("Your input: ");
+                    String decision = scan.nextLine();
+                    decision = decision.toLowerCase();
+                    if (decision.equals("n")) System.exit(0);
+                }
+            } else if (choice.equals("df")){
                 System.out.println("The available faculties:");
                 for (Faculty faculty : faculties) {
                     System.out.println(" - " + faculty.getName());
@@ -55,7 +70,6 @@ public class GeneralOperations {
                 System.out.println();
                 System.out.println();
             } else if (choice.startsWith("df/")){
-                String[] parts = choice.split("/");
                 if (parts.length == 2) {
                     String field = parts[1];
                     if (StudyField.validation(field)) {
--- Laboratory1/Operations/StudentOperations.java
+++ Laboratory1/Operations/StudentOperations.java
@@ -1,5 +1,8 @@
 package Laboratory1.Operations;
 
+import Laboratory1.Faculty;
+import Laboratory1.StudyField;
+
 import java.util.Scanner;
 
 public class StudentOperations {
