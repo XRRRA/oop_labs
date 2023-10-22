e2aa6e4dfcabe21eb336471b648ab4cdd42c943f
XRRRA <liviu.iordan@isa.utm.md>
2023-10-22 22:57:38 +0300

    Commit and status commands finalized, with snapshot saving and loading

--- Laboratory2/Menu.java
+++ Laboratory2/Menu.java
@@ -11,17 +11,17 @@ public class Menu {
 
         Scanner input = new Scanner(System.in);
 
-        String choice = "" ;
+        String choice = "";
         System.out.println();
         System.out.println();
         System.out.println("Welcome to the Document Change Detector system:");
         document.displayLastSnapshotTime();
 
-        while(!choice.equals("q")) {
+        while (!choice.equals("q")) {
             System.out.println();
             System.out.println();
             System.out.println("commit - update snapshot time");
-            System.out.println("info<filename> - find information about the 'filename'");
+            System.out.println("info type/filename  - find information about the 'filename' (type is whether Text, Images or Code)");
             System.out.println("status - display all the changes in the files");
             System.out.println();
             System.out.println("q - to quit the program");
@@ -31,22 +31,26 @@ public class Menu {
                 case "commit":
                     document.commit();
                     break;
-                case "info":
-                    break;
                 case "status":
                     document.status("Laboratory2/files");
                     break;
-                case "q":
-                    System.out.println("Quiting the program");
-                    input.close();
-                    break;
                 default:
-                    System.out.println("Invalid choice. Do you want to try again? y/n");
-                    String decision = input.nextLine().trim().toLowerCase();
-                    if (!decision.equals("y")) {
+                    if (choice.startsWith("info")) {
+                        // Extract filename from the "info<filename>" command
+                        String filename = choice.substring(5).trim();
+                        document.info(filename);
+                    } else if (choice.equals("q")){
                         System.out.println("Quiting the program");
-                        choice = "q";
                         input.close();
+                        break;
+                    } else {
+                        System.out.println("Invalid choice. Do you want to try again? y/n");
+                        String decision = input.nextLine().trim().toLowerCase();
+                        if (!decision.equals("y")) {
+                            System.out.println("Quitting the program");
+                            choice = "q";
+                            input.close();
+                        }
                     }
             }
         }
--- Laboratory2/classes/DocumentChangeDetector.java
+++ Laboratory2/classes/DocumentChangeDetector.java
@@ -1,5 +1,7 @@
 package Laboratory2.classes;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -7,10 +9,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocumentChangeDetector {
     private final ArrayList<Document> documents = new ArrayList<>();
-    private long lastSnapshotTime = loadLastSnapshotTime(); // Load the last snapshot time from a file
+    private long lastSnapshotTime = loadLastSnapshotTime();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public void displayLastSnapshotTime() {
@@ -19,7 +24,7 @@ public class DocumentChangeDetector {
 
     public void commit() {
         lastSnapshotTime = System.currentTimeMillis();
-        saveLastSnapshotTime(lastSnapshotTime); // Save the new snapshot time to a file
+        saveLastSnapshotTime(lastSnapshotTime);
         System.out.println("Snapshot time updated to: " + dateFormat.format(lastSnapshotTime));
     }
 
@@ -29,12 +34,14 @@ public class DocumentChangeDetector {
         Path rootPath = Paths.get(rootFolderPath);
         Files.walkFileTree(rootPath, options, Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
             @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
                 String relativePath = rootPath.relativize(file).toString();
                 long lastModifiedTime = attrs.lastModifiedTime().toMillis();
                 if (lastModifiedTime > lastSnapshotTime) {
                     System.out.println(relativePath + " - Changed (on " + dateFormat.format(new Date(lastModifiedTime)) + ")");
-                } else {System.out.println(relativePath + " - No Change");}
+                } else {
+                    System.out.println(relativePath + " - No Change");
+                }
                 return FileVisitResult.CONTINUE;
             }
         });
@@ -50,7 +57,7 @@ public class DocumentChangeDetector {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return 0; // Default to 0 if the snapshot time file is not found
+        return 0;
     }
 
     private void saveLastSnapshotTime(long lastSnapshotTime) {
@@ -65,21 +72,97 @@ public class DocumentChangeDetector {
     }
 
     public void info(String filename) {
-        for (Document document : documents) {
-            if (document.getName().equals(filename)) {
-                document.printInfo();
-                return;
+        Path filePath = Paths.get("Laboratory2/files", filename);
+        if (Files.exists(filePath)) {
+            try {
+                BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                System.out.println("File Name: " + filePath.getFileName());
+                System.out.println("Created: " + dateFormat.format(new Date(attributes.creationTime().toMillis())));
+                System.out.println("Last Modified: " + dateFormat.format(new Date(attributes.lastModifiedTime().toMillis())));
+
+                if (filename.endsWith(".txt")) {
+                    // Display text file information
+                    displayTextFileInfo(filePath);
+                } else if (filename.endsWith(".img") || filename.endsWith(".jpg")) {
+                    // Display image file information
+                    displayImageFileInfo(filePath);
+                } else if (filename.endsWith(".py") || filename.endsWith(".java")) {
+                    // Display Python or Java file information
+                    displayCodeFileInfo(filePath);
+                } else {
+                    System.out.println("File type not supported for additional information.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("File not found: " + filename);
+        }
+    }
+
+    private void displayTextFileInfo(Path filePath) {
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            int lineCount = lines.size();
+            int charCount = 0;
+            int wordCount = 0;
+
+            for (String line : lines) {
+                charCount += line.length();
+                String[] words = line.split("\\s+");
+                wordCount += words.length;
+            }
+
+            System.out.println("Line Count: " + lineCount);
+            System.out.println("Character Count: " + charCount);
+            System.out.println("Word Count: " + wordCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayImageFileInfo(Path filePath) {
+        try {
+            BufferedImage image = ImageIO.read(filePath.toFile());
+            int width = image.getWidth();
+            int height = image.getHeight();
+            System.out.println("Image Size: " + width + "x" + height);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayCodeFileInfo(Path filePath) {
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            int lineCount = lines.size();
+            int methodCount = countMethods(lines);
+
+            System.out.println("Line Count: " + lineCount);
+            System.out.println("Method Count: " + methodCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int countMethods(List<String> lines) {
+        int methodCount = 0;
+        boolean insideMethod = false;
+        Pattern methodPattern = Pattern.compile(".*(\\b(public|private|protected)\\b)?.*\\bvoid\\s+(\\w+)\\s*\\(.*");
+
+        for (String line : lines) {
+            if (!insideMethod) {
+                Matcher matcher = methodPattern.matcher(line);
+                if (matcher.matches()) {
+                    methodCount++;
+                    insideMethod = true;
+                }
+            } else if (line.trim().isEmpty()) {
+                insideMethod = false;
             }
         }
-        System.out.println("File not found: " + filename);
-    }
 
-    public void addDocument(Document document) {
-        documents.add(document);
+        return methodCount;
     }
-
-    public void removeDocument(Document document) {
-        documents.remove(document);
-    }
-
 }
